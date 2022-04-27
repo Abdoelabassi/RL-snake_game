c6894aa7d08a51cfd3f415585014156434d542fe
Abdoelabassi <abderrezaq@outlook.com>
2022-04-27 16:21:00 +0000

    build model

--- module.py
+++ module.py
@@ -23,10 +23,38 @@ class Linear_QNet(nn.Module):
 
 	def forward(self, x):
 
-		X = Func.rely(self.linear_1(x))
+		X = Func.relu(self.linear_1(x))
 
 		X = self.linear_2(x)
 
 		return X
 
-	def save(self, file_name="model.pth")
+	def save(self, file_name="model.pth"):
+
+		model_folder_path = "./model"
+
+		if not os.path.exists(model_folder_path):
+
+			os.makedirs(model_folder_path)
+
+		file_name = os.path.join(model_folder_path)
+		torch.save(self.state_dict(), final_move)
+
+
+class QTrainer:
+
+	def __init__(self, model, lr, gamma):
+
+		self.lr = lr 
+		self.gamma = gamma
+
+		self.model =  model
+
+		self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
+
+		self.criterion = nn.MSELoss()
+
+
+	def train_step(self, state, action, reward, next_state, done):
+
+		pass
