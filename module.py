ea4bc070a9c98ff42a782f900801b0a2f713457b
Abdoelabassi <abderrezaq@outlook.com>
2022-04-27 17:10:11 +0000

    updating..

--- module.py
+++ module.py
@@ -1,107 +1,70 @@
 import torch
-
 import torch.nn as nn
-
 import torch.optim as optim
-
-import torch.nn.functional as Func
-
+import torch.nn.functional as F
 import os
 
-
 class Linear_QNet(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, output_size)
 
-	def __init__(self, input_size, hidden_size, output_size):
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = self.linear2(x)
+        return x
 
-		super().__init__()
+    def save(self, file_name='model.pth'):
+        model_folder_path = './model'
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
 
-		self.linear_1 = nn.Linear(input_size, hidden_size)
-
-		self.linear_2 = nn.Linear(hidden_size, output_size)
-
-
-
-	def forward(self, x):
-
-		X = Func.relu(self.linear_1(x))
-
-		X = self.linear_2(x)
-
-		return X
-
-	def save(self, file_name="model.pth"):
-
-		model_folder_path = "./model"
-
-		if not os.path.exists(model_folder_path):
-
-			os.makedirs(model_folder_path)
-
-		file_name = os.path.join(model_folder_path)
-		torch.save(self.state_dict(), final_move)
+        file_name = os.path.join(model_folder_path, file_name)
+        torch.save(self.state_dict(), file_name)
 
 
 class QTrainer:
+    def __init__(self, model, lr, gamma):
+        self.lr = lr
+        self.gamma = gamma
+        self.model = model
+        self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
+        self.criterion = nn.MSELoss()
 
-	def __init__(self, model, lr, gamma):
+    def train_step(self, state, action, reward, next_state, done):
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+        reward = torch.tensor(reward, dtype=torch.float)
+        # (n, x)
 
-		self.lr = lr 
-		self.gamma = gamma
+        if len(state.shape) == 1:
+            # (1, x)
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            done = (done, )
 
-		self.model =  model
+        # 1: predicted Q values with current state
+        pred = self.model(state)
 
-		self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
+        target = pred.clone()
+        for idx in range(len(done)):
+            Q_new = reward[idx]
+            if not done[idx]:
+                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
 
-		self.criterion = nn.MSELoss()
-
-
-	def train_step(self, state, action, reward, next_state, done):
-
-		state = torch.tensor(state, dtype=torch.float)
-		next_state = torch.tensor(next_state, dtype=torch.float)
-		action = torch.tensor(action, dtype=torch.long)
-		reward = torch.tensor(reward, dtype=torch.float)
-
-		if len(state.shape) == 1:
-
-			#format : (1,x)
-
-			state = torch.unsqueeze(state, 0)
-
-			next_state = torch.unsqueeze(next_state,0)
-
-			action = torch.unsqueeze(action,0)
-
-			reward = torch.unsqueeze(reward,0)
-
-			done = (done,)
-
-		#Bellman equation: implementation
-		#1:predict Q value of the current state
-
-		pred = self.model(state)
-
-		#2: Q_new = reward + gamma*max(current Q value with the new state) -> only if done
-		#pred[argmax(action)] = Q_new
-
-		tar = pred.clone()
-
-		for i in range(len(done)):
-
-			Q_new = reward[i]
-
-			if not done[i]:
-
-				Q_new = reward[i] + self.gamma* torch.max(self.model(next_state[i]))
-
-		    tar[i][torch.argmax(action).item()] = Q_new
-
-
-		self.optimizer.zero_grad()
+            target[idx][torch.argmax(action[idx]).item()] = Q_new
+    
+        # 2: Q_new = r + y * max(next_predicted Q value) -> only do this if not done
+        # pred.clone()
+        # preds[argmax(action)] = Q_new
+        self.optimizer.zero_grad()
         loss = self.criterion(target, pred)
         loss.backward()
 
         self.optimizer.step()
-
 
 
